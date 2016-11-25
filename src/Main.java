1b05e999e7c3cac6f3ef4ef78c5bbf8cf5d627d5
Raniro Coelho <errecme@gmail.com>
2016-11-25 13:20:01 -0200

    add read file

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         TrieSET set = new TrieSET();
-        set.add("palavra1");
-        set.add("palavra2");
-        set.add("palavra3");
-        set.add("palavra4");
-        set.add("palavra5");
-        set.add("palavra6");
+
+        String file = "src/words.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                set.add(line);
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
 
         for (String key : set){
             System.out.println(key);
         }
 
-        for (String s : set.keysThatMatch("palavra3")) {
-            System.out.println(s);
+        for (String s : set.keysThatMatch("puzzler")) {
+            System.out.println("\nword " + s + " found");
         }
     }
 }
